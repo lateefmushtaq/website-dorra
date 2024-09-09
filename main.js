690d65e27e51cc314920c9badf4078497d1b473a
Lateef Mushtaq <lateef9thncs@gmail.com>
2024-09-09 01:00:58 +0100

    yes

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function updateActiveLink() {
 
     sections.forEach(section => {
         const sectionTop = section.offsetTop;
-        if (window.pageYOffset >= sectionTop - 60) {
+        if (window.scrollY >= sectionTop - 60) {
             currentSection = section.getAttribute('id');
         }
     });
@@ -30,6 +30,20 @@ function updateActiveLink() {
 
 
 
-console.log(substring(1))
+var swiper = new Swiper(`[unique-script-id="w-w-dm-id"] .mySwiper`, {
+    slidesPerView: 1.5,
+    centeredSlides: true,
+    spaceBetween: 5,
+    pagination: {
+        el: `[unique-script-id="w-w-dm-id"] .swiper-pagination`,
+        clickable: true,
+    },
+    loop: true,
+    navigation: {
+        nextEl: `[unique-script-id="w-w-dm-id"] .swiper-button-nexts`,
+        prevEl: `[unique-script-id="w-w-dm-id"] .swiper-button-prevs`,
+    },
+});
+
 
 
